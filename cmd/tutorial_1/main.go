74b2168afb8543185face7e75da992cee2348a15
jpdealba <jpdeaty1207@gmail.com>
2024-02-07 22:06:17 -0600

    functions, parameters and switch statements tutorial

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,39 +1,56 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"unicode/utf8"
 )
 
 func main() {
-	var intNum int = 32767
-	fmt.Println(intNum)
+	var printValue string = "Hello, World!"
+	printMe(printValue)
 
-	var floatNum float32 = 3.141592
-	fmt.Println(floatNum)
+	var numerator int = 11
+	var denominator int = 2
+	var result, remainder, err = intDivision(numerator, denominator)
+	// with if
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// } else if remainder == 0 {
+	// 	fmt.Printf("The result of the integer division is %v", result)
+	// } else {
+	// 	fmt.Printf("The result of the integer division is %v with a remainder of %v\n", result, remainder)
+	// }
+	// with switch statement
+	switch {
+	case err != nil:
+		fmt.Println(err.Error())
+	case remainder == 0:
+		fmt.Printf("The result of the integer division is %v", result)
+	default:
+		fmt.Printf("The result of the integer division is %v with a remainder of %v\n", result, remainder)
+	}
 
-	var str string = "Hello" + " World!"
-	newStr := "Hello"
+	// switch based on remainder
+	switch remainder {
+	case 0:
+		fmt.Println("Integer division, no remainder")
+	default:
+		fmt.Printf("Division with remainder %d\n", remainder)
+	}
 
-	fmt.Println(str)
-	fmt.Println(newStr)
+}
 
-	fmt.Println(len("Hello World!"))
-	fmt.Println(utf8.RuneCountInString("Hello World!"))
+func printMe(printValue string) {
+	fmt.Println(printValue)
+}
 
-	var myRune rune = 'a'
-	fmt.Println(myRune)
-
-	var myBool bool = true
-	fmt.Println(myBool)
-
-	// this is only usefull when dealing when obvious datatypes, but when dealing with func responses or that
-	// are not obvious, it is better to use the var keyword and add the datatype
-	var1, var2 := 1, 2
-	fmt.Println(var1, var2)
-
-	// this is a constant, and it cannot be changed, it has to be initialized
-	const myConst int = 10
-	fmt.Println(myConst)
-
+func intDivision(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("Cannot divide by zero")
+		return 0, 0, err
+	}
+	var result int = numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder, err
 }
